b8fe78fc02adade8c22791db680b65dee7983c44
Cole B <cole.brauer@outlook.com>
2019-08-06 17:52:30 -0700

    Modified mesh import demo to use current plotting commands

--- python/voxelbots_examples/load_stl/load_mesh.py
+++ python/voxelbots_examples/load_stl/load_mesh.py
@@ -4,7 +4,8 @@ Created on Tue Apr 30 15:57:14 2019
 
 @author: daukes
 """
-import idealab_tools.plot_tris
+import PyQt5.QtGui as qg
+import sys
 import meshio
 import numpy
 import subprocess
@@ -14,14 +15,26 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import scipy.signal
 
-def make_mesh(filename,delete_files=True):
+from voxelbots.voxel_model import VoxelModel
+from voxelbots.voxel_model import formatVoxData
+from voxelbots.mesh import Mesh
+from voxelbots.plot import Plot
+from voxelbots.materials import materials
+
+def make_mesh(filename, delete_files=True):
+    template = '''
+    Merge "{0}";
+    Surface Loop(1) = {{1}};
+    //+
+    Volume(1) = {{1}};
+    '''
 
     geo_string=template.format(filename)
     with open('output.geo','w') as f:
         f.writelines(geo_string)
     
     command_string = 'gmsh output.geo -3 -format msh'
-    p = subprocess.Popen(command_string)
+    p = subprocess.Popen(command_string, shell=True)
     p.wait()
     mesh_file = 'output.msh'
     data = meshio.read(mesh_file)
@@ -30,91 +43,72 @@ def make_mesh(filename,delete_files=True):
         os.remove('output.geo')
     return data
 
-def generate_outer(v):
-    kernel = numpy.zeros((3,3,3))
-    kernel[:,1,1]=1
-    kernel[1,:,1]=1
-    kernel[1,1,:]=1
-    
-    v2 = scipy.signal.convolve(voxels*1,kernel,mode='same')
-    v2 = v2.round()
-    v2 = v&(v2<len(kernel.nonzero()[0]))
-    return v2,kernel
+if __name__=='__main__':
+    app1 = qg.QApplication(sys.argv)
 
-template = '''
-Merge "{0}";
-Surface Loop(1) = {{1}};
-//+
-Volume(1) = {{1}};
-'''
+    res = 0
 
-res = 0
+    data = make_mesh('axes.stl',True)
 
-data = make_mesh('tube.stl',True)
+    points = data.points
+    ii_tri = data.cells['triangle']
+    ii_tet = data.cells['tetra']
 
-points = data.points
-ii_tri = data.cells['triangle']
-ii_tet = data.cells['tetra']
-
-tris= points[ii_tri]
-tets= points[ii_tet]
-T=numpy.concatenate((tets,tets[:,:,0:1]*0+1),2)
-T_inv = numpy.zeros(T.shape)
-for ii,t in enumerate(T):
-    T_inv[ii] = numpy.linalg.inv(t).T
+    tris= points[ii_tri]
+    tets= points[ii_tet]
+    T=numpy.concatenate((tets,tets[:,:,0:1]*0+1),2)
+    T_inv = numpy.zeros(T.shape)
+    for ii,t in enumerate(T):
+        T_inv[ii] = numpy.linalg.inv(t).T
 
 
-points_min = points.min(0)
-points_max = points.max(0)
+    points_min = points.min(0)
+    points_max = points.max(0)
 
-points_min_r = numpy.round(points_min,res)
-points_max_r = numpy.round(points_max,res)
+    points_min_r = numpy.round(points_min,res)
+    points_max_r = numpy.round(points_max,res)
 
-xx = numpy.r_[points_min_r[0]:points_max_r[0]:10**(-res)]
-yy = numpy.r_[points_min_r[1]:points_max_r[1]:10**(-res)]
-zz = numpy.r_[points_min_r[2]:points_max_r[2]:10**(-res)]
+    xx = numpy.r_[points_min_r[0]:points_max_r[0]:10**(-res)]
+    yy = numpy.r_[points_min_r[1]:points_max_r[1]:10**(-res)]
+    zz = numpy.r_[points_min_r[2]:points_max_r[2]:10**(-res)]
 
-xx_mid = (xx[1:]+xx[:-1])/2
-yy_mid = (yy[1:]+yy[:-1])/2
-zz_mid = (zz[1:]+zz[:-1])/2
+    xx_mid = (xx[1:]+xx[:-1])/2
+    yy_mid = (yy[1:]+yy[:-1])/2
+    zz_mid = (zz[1:]+zz[:-1])/2
 
-xyz_mid = numpy.array(numpy.meshgrid(xx_mid,yy_mid,zz_mid,indexing='ij'))
-xyz_mid = xyz_mid.transpose(1,2,3,0)
-xyz_mid = xyz_mid.reshape(-1,3)
-xyz_mid = numpy.concatenate((xyz_mid,xyz_mid[:,0:1]*0+1),1)
+    xyz_mid = numpy.array(numpy.meshgrid(xx_mid,yy_mid,zz_mid,indexing='ij'))
+    xyz_mid = xyz_mid.transpose(1,2,3,0)
+    xyz_mid = xyz_mid.reshape(-1,3)
+    xyz_mid = numpy.concatenate((xyz_mid,xyz_mid[:,0:1]*0+1),1)
 
-ijk_mid = numpy.array(numpy.meshgrid(numpy.r_[:len(xx_mid)],numpy.r_[:len(yy_mid)],numpy.r_[:len(zz_mid)],indexing='ij'))
-ijk_mid = ijk_mid.transpose(1,2,3,0)
-ijk_mid2 = ijk_mid.reshape(-1,3)
-#
-u2 = T_inv.dot(xyz_mid.T)
+    ijk_mid = numpy.array(numpy.meshgrid(numpy.r_[:len(xx_mid)],numpy.r_[:len(yy_mid)],numpy.r_[:len(zz_mid)],indexing='ij'))
+    ijk_mid = ijk_mid.transpose(1,2,3,0)
+    ijk_mid2 = ijk_mid.reshape(-1,3)
+    #
+    u2 = T_inv.dot(xyz_mid.T)
 
-f1 = ((u2[:,:,:]>=0).sum(1)==4)
-f2 = ((u2[:,:,:]<=1).sum(1)==4)
-f3 = f1&f2
-ii,jj = f3.nonzero()
+    f1 = ((u2[:,:,:]>=0).sum(1)==4)
+    f2 = ((u2[:,:,:]<=1).sum(1)==4)
+    f3 = f1&f2
+    ii,jj = f3.nonzero()
 
-#print(len(jj))
-#print(len(numpy.unique(jj)))
+    #print(len(jj))
+    #print(len(numpy.unique(jj)))
 
-lmn = ijk_mid2[numpy.unique(jj)]
+    lmn = ijk_mid2[numpy.unique(jj)]
 
-voxels = numpy.zeros(ijk_mid.shape[:3],dtype=numpy.bool)
-voxels[lmn[:,0],lmn[:,1],lmn[:,2]]=True
-v2,kernel = generate_outer(voxels)
-colors = np.empty(v2.shape, dtype=object)
-colors[v2] = 'red'
+    voxels = numpy.zeros(ijk_mid.shape[:3],dtype=numpy.bool)
+    voxels[lmn[:,0],lmn[:,1],lmn[:,2]]=True
 
-colors2 = np.empty(kernel.shape, dtype=object)
-colors2[kernel==1] = 'red'
+    modelData = formatVoxData(voxels, len(materials))
+    model = VoxelModel(modelData)
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.voxels(kernel, facecolors=colors2, edgecolor='k')
+    # Create mesh data
+    mesh1 = Mesh.fromVoxelModel(model)
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.voxels(v2, facecolors=colors, edgecolor='k')
+    # Create plot
+    plot1 = Plot(mesh1, grids=True)
+    plot1.show()
 
-#for tri in tris:
-#    ax.plot3D(tri[(0,1,2,0),0],tri[(0,1,2,0),1],tri[(0,1,2,0),2],'k-')
+    app1.processEvents()
+    app1.exec_()
