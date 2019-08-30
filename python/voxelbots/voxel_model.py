ec89d03f33e09f6372e0d499444d778461d3a149
jnbrauer <jnbrauer@outlook.com>
2019-08-29 18:50:52 -0700

    Removed duplicated code

--- python/voxelbots/voxel_model.py
+++ python/voxelbots/voxel_model.py
@@ -11,6 +11,7 @@ from voxelbots.materials import materials
 from scipy import ndimage
 from numba import njit, prange
 import pyopencl as cl
+from voxelbots.opencl2 import opencl_dot3d
 
 """
 VoxelModel Class
@@ -647,7 +648,7 @@ def formatVoxData(input_matrix, material_count):
     y_len = len(input_matrix[:, 0, 0])
     z_len = len(input_matrix[0, :, 0])
 
-    new_model = np.zeros((y_len, z_len, x_len, material_count+1), dtype=float16)
+    new_model = np.zeros((y_len, z_len, x_len, material_count+1), dtype=np.float16)
 
     # Loop through input_model data
     for x in range(x_len):
@@ -710,74 +711,3 @@ def dither(model):
                             model[y, z, x+1, i] += error * (3/10) * model[y, z, x+1, 0]
 
     return model
-
-@njit
-def flatten_3d_to_2d(a):
-    x_len = len(a[:, 0, 0])
-    y_len = len(a[0, :, 0])
-    z_len = len(a[0, 0, :])
-
-    return a.reshape((x_len * y_len, z_len))
-
-def opencl_dot3d(a, b):
-    x_len = np.int32(len(a[:, 0, 0]))
-    y_len = np.int32(len(a[0, :, 0]))
-    z_len = np.int32(len(b[:, 0]))
-
-    a_flat = flatten_3d_to_2d(a)
-
-    print(x_len)
-    print(y_len)
-    print(z_len)
-
-    platform = cl.get_platforms()[0]
-    print(platform.name)
-
-    device = platform.get_devices()[0]
-    print(device.name)
-
-    context = cl.Context([device])
-    # context = cl.create_some_context()
-
-    program = cl.Program(context, """
-        __kernel void matrix_dot_vector(__global const float4 *a, __global const float4 *b, const unsigned int x_len, const int y_len, const int z_len, __global float *result) {
-            int gid = get_global_id(0);
-            int i = gid % (z_len * 2);
-            int j = gid / (x_len * y_len);
-            result[gid] = dot(a[i], b[j]);
-            //result[gid] = b[z].s0;
-            //result[gid] = a[y].s0;
-        }
-    """).build()
-
-    queue = cl.CommandQueue(context)
-
-    mem_flags = cl.mem_flags
-    a_buf = cl.Buffer(context, mem_flags.READ_ONLY | mem_flags.COPY_HOST_PTR, hostbuf=a_flat)
-    b_buf = cl.Buffer(context, mem_flags.READ_ONLY | mem_flags.COPY_HOST_PTR, hostbuf=b)
-    # x_len_buf = cl.Buffer(context, mem_flags.READ_ONLY | mem_flags.COPY_HOST_PTR, hostbuf=np.int32(x_len))
-    # y_len_buf = cl.Buffer(context, mem_flags.READ_ONLY | mem_flags.COPY_HOST_PTR, hostbuf=np.int32(y_len))
-    # z_len_buf = cl.Buffer(context, mem_flags.READ_ONLY | mem_flags.COPY_HOST_PTR, hostbuf=np.int32(z_len))
-
-    result = np.zeros((x_len * y_len * z_len), np.float32)
-    # result = np.zeros(4, np.float32)
-    result_buf = cl.Buffer(context, mem_flags.WRITE_ONLY, result.nbytes)
-
-    program.matrix_dot_vector(queue, result.shape, None, a_buf, b_buf, x_len, y_len, z_len, result_buf)
-
-    cl.enqueue_copy(queue, result, result_buf)
-
-    print(len(result))
-
-    #charles' crappy attempt at a deflattener
-    unflattened = np.zeros((x_len, y_len, z_len), dtype=np.float32)
-    index_result = 0
-    for z in prange(z_len):
-        for x in prange(x_len):
-            for y in prange(y_len):
-                unflattened[x, y, z] = result[index_result]
-                index_result += 1
-
-    #return result
-    return unflattened
-
