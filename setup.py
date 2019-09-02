bbf462bf7e225091defac333d8967f996f6cb95f
Cole B <cole.brauer@outlook.com>
2019-09-02 13:40:59 -0700

    Added description to setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ setuptools.setup(
     name="voxelfuse",
     version="1.0.0",
     author="Cole Brauer",
+    description="A toolkit for processing 3D components made with mixtures of materials and multiple manufacturing processes",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Team-Automata/VoxelFuse",
+    url="https://github.com/cdbrauer/VoxelFuse",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
