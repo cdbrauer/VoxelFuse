554ff3a4971417b7a6a1939110801174d7cf1514
Charles <sazrocks6@gmail.com>
2019-10-03 23:26:50 -0700

    initial attempts to get the new import working in opencl

--- voxelfuse/opencl.py
+++ voxelfuse/opencl.py
@@ -118,6 +118,70 @@ def opencl_dot3d(a, b):
     return result
     # return unflattened
 
+
+def opencl_dot2d(a, b):
+    a = a.astype(np.float32)
+    b = b.astype(np.float32)
+
+    y_len = np.int32(len(a[:, 0]))
+    z_len = np.int32(len(b[:, 0]))
+
+    print(y_len)
+    print(z_len)
+
+    platform = cl.get_platforms()[0]
+    print(platform.name)
+
+    device = platform.get_devices()[0]
+    print(device.name)
+
+    context = cl.Context([device])
+
+    program = cl.Program(context, """
+        __kernel void matrix_dot_vector(__global const float4 *a, __global const float4 *b, const unsigned int x_len, const int y_len, const int start, __global float *result) {
+            int gid = get_global_id(0);
+            int i = gid + start;
+
+            int z = i / (x_len * y_len);
+            int l = i % (x_len * y_len);
+            int y = l / x_len;
+            int x = l % x_len;
+
+            result[gid] = dot(a[y*x_len + x], b[z]);
+        }
+    """).build()
+
+    queue = cl.CommandQueue(context)
+
+    mem_flags = cl.mem_flags
+    a_buf = cl.Buffer(context, mem_flags.READ_ONLY | mem_flags.COPY_HOST_PTR, hostbuf=a)
+    b_buf = cl.Buffer(context, mem_flags.READ_ONLY | mem_flags.COPY_HOST_PTR, hostbuf=b)
+
+    result_len = x_len * y_len * z_len
+
+    # TODO: Calculate how many parts we need to split the result into in order to not run out of memory
+    n_parts = 1
+
+    # result = np.array([], np.float32)
+    result = np.zeros((x_len, y_len, z_len), np.float32)
+
+    for i in range(n_parts):
+        start = np.int32(result_len - (result_len // (i + 1)))
+        result_part = np.zeros(result_len // n_parts, np.float32)
+
+        result_buf = cl.Buffer(context, mem_flags.WRITE_ONLY, result_part.nbytes)
+        program.matrix_dot_vector(queue, result_part.shape, None, a_buf, b_buf, x_len, y_len, start, result_buf)
+        cl.enqueue_copy(queue, result_part, result_buf)
+
+        # result.append(result_part)
+        print("Adding")
+        # result = np.append(result, result_part, axis=0)
+        unflatten_and_append(result, result_part, start)
+        print("Done adding")
+
+
+    return result
+
 @njit(parallel=True)
 def numba_dot3d(a, b):
     x_len = len(a[:, 0, 0])
