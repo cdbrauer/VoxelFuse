211cacedc38a351326a5e4884524f3fa6c50e355
cdbrauer <cole.brauer@outlook.com>
2020-06-12 09:25:03 -0700

    Added alternate resolution options for collision_sim_1 example

--- voxelfuse_examples/voxelyze_integration/collision_sim_1.py
+++ voxelfuse_examples/voxelyze_integration/collision_sim_1.py
@@ -7,15 +7,17 @@ Export a simulation of a single falling object
 
 from voxelfuse.voxel_model import VoxelModel
 from voxelfuse.primitives import cube, cuboid
-from  voxelfuse.simulation import Simulation
+from voxelfuse.simulation import Simulation, StopCondition
 
 if __name__=='__main__':
-    cubeModel = cube(5, (0, 0, 10), material=5)
-    planeModel = cuboid((9, 9, 1), (-2, -2, 0), material=5)
+    cubeModel = cube(5, (0, 0, 10), material=4)
+    planeModel = cuboid((9, 9, 1), (-2, -2, 0), material=4)
 
     modelResult = planeModel | cubeModel
+    modelResult = modelResult.scale(1)
     modelResult = modelResult.removeDuplicateMaterials()
 
     simulation = Simulation(modelResult)  # Initialize a simulation
     simulation.setCollision() # Enable self-collision
+    simulation.setStopCondition(StopCondition.TIME_VALUE, 0.01)
     simulation.launchSim('collision_sim_1', delete_files=False) # Launch simulation, save simulation file
