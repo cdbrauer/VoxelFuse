b50964d95c3efddd1135c1ef91232c6fb771fb1b
cdbrauer <cole.brauer@outlook.com>
2020-06-20 19:48:14 -0700

    First test of implementing topology optimization

---
+++ voxelfuse/optimization.py
@@ -0,0 +1,107 @@
+"""
+Functions for performing various optimization algorithms.
+
+----
+
+Copyright 2020 - Cole Brauer, Dan Aukes
+"""
+
+import numpy as np
+from enum import Enum
+from typing import List
+from voxelfuse.voxel_model import VoxelModel
+from voxelfuse.simulation import Simulation
+from voxelfuse.materials import material_properties
+
+class SimResult(Enum):
+    """
+    Options for simulation result measurements.
+    """
+    FACTOR_OF_SAFETY = 'SafetyFactor'
+    STRESS = 'BondStress'
+    PRESSURE = 'Pressure'
+
+def optimizeTopology(simulation, condition: SimResult, value: float, removal_tolerance: float = 0, max_iter: int = 10, protected = None):
+    """
+    Remove material from a model until a threshold value is reached.
+
+    This currently works only for measurements that must stay ABOVE the threshold (i.e. FOS).  Measurements that
+    must stay BELOW the threshold (e.g. stress, pressure) still need to be added.
+
+    :param simulation: Simulation object with desired loads and constraints
+    :param condition: Measurement to monitor, set with SimResult class
+    :param value: Measurement threshold value
+    :param removal_tolerance: Sets how close voxels must be to the max/min value in order to be removed
+    :param max_iter: Maximum number of material removal iterations
+    :param protected: Material types that should not be removed
+    :return: VoxelModel
+    """
+    if protected is None:
+        protected = []
+
+    # Copy simulation and model
+    new_sim = Simulation.copy(simulation)
+    new_model = VoxelModel.copy(simulation.getModel())
+
+    x_len = len(new_model.voxels[:, 0, 0])
+    y_len = len(new_model.voxels[0, :, 0])
+    z_len = len(new_model.voxels[0, 0, :])
+
+    firstIter = True
+    last_max = 0.0
+    last_min = 0.0
+
+    for i in range(max_iter):
+        # Clear any existing sensors
+        new_sim.clearSensors()
+
+        # Add sensors to all voxels that are not empty or protected
+        for x in range(x_len):
+            for y in range(y_len):
+                for z in range(z_len):
+                    mat = new_model.voxels[x, y, z]
+                    if mat != 0 and mat not in protected:
+                        new_sim.addSensor((x, y, z))
+
+        # Run simulation
+        new_sim.runSim(voxelyze_on_path=True)
+
+        # Generate array of result values
+        result_values = np.zeros_like(new_model.voxels)
+        for s in range(len(new_sim.results)):
+            x = new_sim.results[s]['Location'][0]
+            y = new_sim.results[s]['Location'][1]
+            z = new_sim.results[s]['Location'][2]
+            try:
+                result_values[x, y, z] = new_sim.results[s][condition.value]
+            except OverflowError:
+                print('Overflow error')
+
+        # Find max/min value
+        max_result_value = np.max(result_values[np.nonzero(result_values)])
+        min_result_value = np.min(result_values[np.nonzero(result_values)])
+
+        if firstIter:
+            firstIter = False
+        else:
+            max_result_value = min(max_result_value, last_max)
+            min_result_value = min(min_result_value, last_min)
+
+        print('Min value: ' + str(min_result_value))
+        print('Max value: ' + str(max_result_value))
+
+        # If max/min value has not reached the limiting value
+        if min_result_value > value:
+            new_model.voxels[result_values >= (max_result_value - removal_tolerance)] = 0 # Remove voxel with min/max value
+            new_sim.setModel(new_model) # Update simulation
+        else:
+            print('Goal condition reached in ' + str(i+1) + ' iterations')
+            break
+
+        if i+1 == max_iter:
+            print('Max iterations reached')
+
+        last_max = max_result_value
+        last_min = min_result_value
+
+    return new_model
---
+++ voxelfuse_examples/topology_optimization/beam_optimization.py
@@ -0,0 +1,39 @@
+"""
+Perform topology optimization on a simple beam model.
+
+----
+
+Copyright 2020 - Cole Brauer, Dan Aukes
+"""
+
+# Import Libraries
+from voxelfuse.voxel_model import VoxelModel
+from voxelfuse.simulation import Simulation, StopCondition
+from voxelfuse.optimization import optimizeTopology, SimResult
+
+# Start Application
+if __name__ == '__main__':
+    # Import Model
+    modelName = 'beam1'
+    beamModel = VoxelModel.fromVoxFile(modelName + '.vox')
+    beamModel = beamModel.translate((0, 0, 15))
+
+    # Create simulation
+    simulation = Simulation(beamModel)
+
+    # Set simulation stop conditions
+    simulation.setEquilibriumMode()
+    simulation.setStopCondition(StopCondition.MOTION_FLOOR, 1e-05)
+
+    # Apply loads and constraints
+    simulation.addBoundaryConditionBox(position=(0, 0, 0), size=(0.01, 1, 1))
+    simulation.addBoundaryConditionBox(position=(0.99, 0, 0), size=(0.01, 1, 1), fixed_dof=0b000000, force=(0, 0, -10))
+
+    # Run optimization
+    newBeamModel = optimizeTopology(simulation, SimResult.FACTOR_OF_SAFETY, 15, removal_tolerance=10, max_iter=50, protected=[1])
+
+    # Export updated model
+    simulation.setModel(newBeamModel)
+    simulation.setEquilibriumMode(False)
+    simulation.setStopCondition(StopCondition.NONE)
+    simulation.launchSim(modelName, delete_files=False)
