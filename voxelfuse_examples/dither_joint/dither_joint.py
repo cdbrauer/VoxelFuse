24d2dfafe80059ea7e4a6e70ee75c69710466613
cdbrauer <cole.brauer@outlook.com>
2020-08-09 14:20:10 -0700

    Updated documentation

--- voxelfuse_examples/dither_joint/dither_joint.py
+++ voxelfuse_examples/dither_joint/dither_joint.py
@@ -11,21 +11,22 @@ Copyright 2020 - Cole Brauer, Dan Aukes
 from voxelfuse.voxel_model import VoxelModel
 from voxelfuse.mesh import Mesh
 
-# User preferences
-modelName = 'joint2.2.vox'
-blurRadius = 10
+if __name__=='__main__':
+    # User preferences
+    modelName = 'joint2.2.vox'
+    blurRadius = 10
 
-# Import model
-modelIn = VoxelModel.fromVoxFile(modelName)
+    # Import model
+    modelIn = VoxelModel.fromVoxFile(modelName)
 
-# Apply blur
-modelBlur = modelIn.blur(blurRadius)
+    # Apply blur
+    modelBlur = modelIn.blur(blurRadius)
 
-# Apply dither
-modelDither = modelBlur.dither(blur=False) # Blur operation separated for clarity
+    # Apply dither
+    modelDither = modelBlur.dither(blur=False) # Blur operation separated for clarity
 
-# Create mesh data
-for m in range(1, len(modelDither.materials)):
-    currentMaterial = modelDither.isolateMaterial(m)
-    currentMesh = Mesh.fromVoxelModel(currentMaterial)
-    currentMesh.export('output_' + str(m) + '.stl')
+    # Create mesh data
+    for m in range(1, len(modelDither.materials)):
+        currentMaterial = modelDither.isolateMaterial(m)
+        currentMesh = Mesh.fromVoxelModel(currentMaterial)
+        currentMesh.export('output_' + str(m) + '.stl')
--- voxelfuse_examples/infill/infill.py
+++ voxelfuse_examples/infill/infill.py
@@ -13,34 +13,35 @@ from voxelfuse.primitives import sphere
 from voxelfuse.periodic import gyroid
 from voxelfuse.mesh import Mesh
 
-# User preferences
-modelRadius = 60
-infillScale = 30
-infillThickness = 1
-shellThickness = 1
+if __name__=='__main__':
+    # User preferences
+    modelRadius = 60
+    infillScale = 30
+    infillThickness = 1
+    shellThickness = 1
 
-# Create volume model
-volume = sphere(radius=modelRadius, material=3)
-volume = volume.setCoords((0,0,0))
+    # Create volume model
+    volume = sphere(radius=modelRadius, material=3)
+    volume = volume.setCoords((0,0,0))
 
-# Create infill structure
-infillN, infillP = gyroid(size=(modelRadius*2, modelRadius*2, modelRadius*2), scale=infillScale)
-infillN = infillN.dilate(infillThickness)
-infillP = infillP.dilate(infillThickness)
-infill = infillN & infillP
+    # Create infill structure
+    infillN, infillP = gyroid(size=(modelRadius*2, modelRadius*2, modelRadius*2), scale=infillScale)
+    infillN = infillN.dilate(infillThickness)
+    infillP = infillP.dilate(infillThickness)
+    infill = infillN & infillP
 
-# Hollow out volume model
-hollow = volume.erode(shellThickness)
-shell = volume.difference(hollow)
+    # Hollow out volume model
+    hollow = volume.erode(shellThickness)
+    shell = volume.difference(hollow)
 
-# Trim infill model
-infill = infill & hollow
+    # Trim infill model
+    infill = infill & hollow
 
-# Combine infill and shell
-result = shell | infill
+    # Combine infill and shell
+    result = shell | infill
 
-# Create mesh data
-for m in range(1, len(result.materials)):
-    currentMaterial = result.isolateMaterial(m)
-    currentMesh = Mesh.fromVoxelModel(currentMaterial)
-    currentMesh.export('output_' + str(m) + '.stl')
+    # Create mesh data
+    for m in range(1, len(result.materials)):
+        currentMaterial = result.isolateMaterial(m)
+        currentMesh = Mesh.fromVoxelModel(currentMaterial)
+        currentMesh.export('output_' + str(m) + '.stl')
