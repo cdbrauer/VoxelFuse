572c8e3019e10f99355b1c17fd8f4c670a19a3f9
cdbrauer <cole.brauer@outlook.com>
2021-05-22 00:35:07 -0700

    Added marching cubes function

--- voxelfuse_examples/primitive_generation/primitive_generation.py
+++ voxelfuse_examples/primitive_generation/primitive_generation.py
@@ -18,7 +18,7 @@ if __name__=='__main__':
     # Save start time
     t1 = time.time()
 
-    app1 = qg.QApplication(sys.argv)
+    # app1 = qg.QApplication(sys.argv)
 
     model1 = cube(11, (0, 0, 0), 1)
     model2 = cuboid((11, 20, 6), (13, 0, 0), 2)
@@ -29,18 +29,19 @@ if __name__=='__main__':
 
     model_result = model1 | model2 | model3 | model4 | model5 | model6
 
-    # Create mesh data
-    mesh1 = Mesh.fromVoxelModel(model_result)
-    mesh1.export('primitives.stl')
+    # Save mesh data
+    # model_result.marchingCubes('primitives', smooth=False)
+    model_result.marchingCubes(None, smooth=False, display=True)
+    model_result.marchingCubes(None, smooth=True, display=True)
 
     # Create plot
-    plot1 = Plot(mesh1, grids=True)
-    plot1.show()
-    app1.processEvents()
+    # plot1 = Plot(mesh1, grids=True)
+    # plot1.show()
+    # app1.processEvents()
 
     # Get elapsed time
-    t2 = time.time()
-    time_pyqt5 = t2 - t1
-    print('Time to generate plot: ' + str(time_pyqt5) + ' sec')
-
-    app1.exec()
+    # t2 = time.time()
+    # time_pyqt5 = t2 - t1
+    # print('Time to generate plot: ' + str(time_pyqt5) + ' sec')
+    #
+    # app1.exec()
